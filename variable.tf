8e0fbc7d1e5c77644f53cff838b2466030412173
Funmi123 <ajanakueniola@gmail.com>
2025-12-08 15:21:17 +0100

    Updated variable.tf

--- variable.tf
+++ variable.tf
@@ -1,4 +1,4 @@
-variable "instance_type" {}
-variable "key_name" {}
-variable "ami" {}
-variable "az_number1" {}
+variable "instance_type" { type = string}
+variable "key_name" { type = string}
+variable "ami_id" { type = string}
+variable "az_number1" { type = string}
